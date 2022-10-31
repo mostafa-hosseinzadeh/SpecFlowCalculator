8fe23b1a2825a277edf5251e8606f723656943c5
Mostafa Hosseinzadeh <mostafa.hosseinzadeh@gmail.com>
2022-10-31 12:41:53 +0330

    InitialStructure

---
+++ CalculatorCore/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculatorCore
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
